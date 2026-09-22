9cd394fe8966a071101921052cc609e199ce451e
Noah <s2401156@edu.bc.fi>
2026-09-22 10:46:36 +0300

    Tehtävä 5 valmis

--- validation.test.js
+++ validation.test.js
@@ -32,3 +32,9 @@ test("validateAge hyväksyy iän 18", () => {
 
   assert.strictEqual(result, true);
 });
+
+test("validateAge hylkää iän 15", () => {
+  const result = validateAge(15);
+
+  assert.strictEqual(result, false);
+});
